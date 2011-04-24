c41c2559ded6fd7b5269c250e9073ab7189c5331
Nic Haynes <nic@nicinabox.com>
2011-04-23 20:44:07 -0500

    iphone checkbox fix

--- public/js/application.js
+++ public/js/application.js
@@ -9,6 +9,20 @@ $(document).ready(function() {
     }
   })
   
+  // iPhone checkbox fix
+  if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/iPad/i)) {
+  	$(document).ready(function () {
+  		$('label[for]').click(function () {
+  			var el = $(this).attr('for');
+  			if ($('#' + el + '[type=radio], #' + el + '[type=checkbox]').attr('selected', !$('#' + el).attr('selected'))) {
+  				return;
+  			} else {
+  				$('#' + el)[0].focus();
+  			}
+  		});
+  	});
+  }
+  
   // help
   $('#tagline').click(function() {
     $('#help').fadeToggle()
